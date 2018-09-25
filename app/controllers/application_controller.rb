012f3e8611fa9696a68e9fd408d0f3c9a9295e5d
Ryan Brewster <brewster1134@gmail.com>
2018-09-24 23:17:52 -0700

    cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
-  before_action :set_current_user
+  after_action :verify_authorized, unless: :devise_controller?
 
-  def set_current_user
-    User.current_user = current_user
-  end
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def default_url_options
     { host: request.host_with_port }
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to do that.'
+    redirect_to root_path
   end
 end
