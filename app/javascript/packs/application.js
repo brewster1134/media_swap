ac0385abd6cac3b5ab9b6030ab2cf2074658b014
Ryan Brewster <brewster1134@gmail.com>
2018-10-02 19:39:14 -0700

    removed console log

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ function isTouchDevice() {
   return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
 }
 
-console.log($('html'), jQuery('body'));
-
 if (isTouchDevice()) {
   $('html').addClass('touch');
 }
